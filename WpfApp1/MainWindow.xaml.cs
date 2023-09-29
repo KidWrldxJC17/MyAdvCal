74063e4f4ce6f6e00224fe9139d1e45dd1926601
KidWrldxJC17 <120431451+KidWrldxJC17@users.noreply.github.com>
2023-09-29 00:35:00 -0500

    Added more Essentials
    There is now a more defined Grid system and layout allowing me to have a more dynamic application. Functional window buttons are near finished, one more feature I would like to add to the minimize and maximize

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,13 +20,34 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             // Closes the application
             Application.Current.Shutdown();
+        }
+
+
+        private void Mini_Click(object sender, RoutedEventArgs e)
+        {
+            // Minimizes the window
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Max_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+            {
+                //The window is already big, make it normal
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                //The window is not big so it Maximizes the window
+                this.WindowState = WindowState.Maximized;
+            }
         }
     }
 }
